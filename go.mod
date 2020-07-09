8872ca69eefdf268820f0ea4ec1f1c905925ad36
Ignacio Vaquero Guisasola <ignacio.vaquero.guisasola@ing.com>
2020-07-09 08:13:55 +0200

    Added telegram notifications

--- go.mod
+++ go.mod
@@ -7,4 +7,6 @@ replace github.com/igvaquero18/go-justwatch => ../go-justwatch
 require (
 	github.com/aws/aws-lambda-go v1.17.0
 	github.com/igvaquero18/go-justwatch v0.0.0-20200623061403-203672add067
+	go.uber.org/zap v1.15.0
+	github.com/igvaquero18/telegram-notifier v0.0.0-20200709053438-7033b25bd928
 )
--- newmovies/main.go
+++ newmovies/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -11,9 +11,30 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/igvaquero18/go-justwatch"
+	"github.com/igvaquero18/telegram-notifier/telegram"
+	"go.uber.org/zap"
 )
 
-const timeFormat string = "2006-01-02"
+const (
+	providersEnv     string = "JUSTWATCH_PROVIDERS"
+	contentTypesEnv  string = "JUSTWATCH_CONTENT_TYPES"
+	verboseEnv       string = "JUSTWATCH_VERBOSE"
+	fromDaysEnv      string = "JUSTWATCH_FROM_DAYS"
+	telegramTokenEnv string = "JUSTWATCH_TELEGRAM_TOKEN"
+	timeFormat       string = "2006-01-02"
+)
+
+var (
+	providers      []string = strings.Split(os.Getenv(providersEnv), ",")
+	contentTypes   []string = strings.Split(os.Getenv(contentTypesEnv), ",")
+	telegramToken  string   = os.Getenv(telegramTokenEnv)
+	fromDays       int
+	chatID         int64
+	jwClient       *justwatch.Client
+	telegramClient *telegram.Client
+	sugar          *zap.SugaredLogger
+	verbose        bool
+)
 
 func getOrElse(envVar, defaultValue string) string {
 	value := os.Getenv(envVar)
@@ -23,22 +44,61 @@ func getOrElse(envVar, defaultValue string) string {
 	return value
 }
 
+func init() {
+	var err error
+
+	verbose, err = strconv.ParseBool(getOrElse(os.Getenv(verboseEnv), "false"))
+
+	if err != nil {
+		verbose = false
+	}
+
+	var zl *zap.Logger
+	cfg := zap.Config{
+		Development: false,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding:         "json",
+		EncoderConfig:    zap.NewProductionEncoderConfig(),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+	if verbose {
+		cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	} else {
+		cfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	}
+	zl, err = cfg.Build()
+
+	if err != nil {
+		log.Fatalf("Error when initializing logger: %s", err.Error())
+	}
+
+	sugar = zl.Sugar()
+	sugar.Debug("Logger initialization successful")
+
+	fromDays, err = strconv.Atoi(getOrElse(fromDaysEnv, "1"))
+	if err != nil {
+		log.Fatalf("Error when converting string to integer: %s", err.Error())
+	}
+
+	jwClient, err = justwatch.NewClient()
+	if err != nil {
+		log.Fatalf("Error when creating new JustWatch client: %s", err.Error())
+	}
+
+	telegramClient, err = telegram.NewClient(telegramToken, sugar)
+	if err != nil {
+		sugar.Fatalf("Error when creating the Telegram client: %s", err.Error())
+	}
+}
+
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, event events.CloudWatchEvent) error {
-	providers := strings.Split(os.Getenv("JUSTWATCH_PROVIDERS"), ",")
-	contentTypes := strings.Split(os.Getenv("JUSTWATCH_CONTENT_TYPES"), ",")
-	fromDays, err := strconv.Atoi(getOrElse("JUSTWATCH_FROM_DAYS", "1"))
-
-	if err != nil {
-		return err
-	}
-
-	client, err := justwatch.NewClient()
-	if err != nil {
-		return err
-	}
-
-	response, err := client.SearchNew(&justwatch.SearchQuery{
+	sugar.Debugw("Executing search query", "providers", providers, "content types", contentTypes)
+	response, err := jwClient.SearchNew(&justwatch.SearchQuery{
 		Providers:    providers,
 		ContentTypes: contentTypes,
 	})
@@ -55,7 +115,7 @@ func Handler(ctx context.Context, event events.CloudWatchEvent) error {
 		if date.After(time.Now().Add(-24 * time.Duration(fromDays) * time.Hour)) {
 			for _, provider := range day.Providers {
 				for _, item := range provider.Items {
-					fmt.Println(item)
+					telegramClient.SendNotification("Nueva peli en Movistar", item.String(), []int64{chatID})
 				}
 			}
 		}
